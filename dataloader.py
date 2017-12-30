8d7137aa6d4762de0950f8b218b616e7b595df65
hysts <hysts@users.noreply.github.com>
2017-12-30 16:01:09 +0900

    Change to apply more data augmentation to FashionMNIST

--- dataloader.py
+++ dataloader.py
@@ -111,12 +111,8 @@ class MNIST(Dataset):
     def __init__(self, config):
         super(MNIST, self).__init__(config)
 
-        if config['dataset'] == 'MNIST':
-            self.mean = np.array([0.1307])
-            self.std = np.array([0.3081])
-        elif config['dataset'] == 'FashionMNIST':
-            self.mean = np.array([0.2860])
-            self.std = np.array([0.3530])
+        self.mean = np.array([0.1307])
+        self.std = np.array([0.3081])
 
         self.train_transform = self._get_train_transform()
         self.test_transform = self._get_default_transform()
@@ -157,6 +153,58 @@ class MNIST(Dataset):
         return self._get_default_transform()
 
 
+class FashionMNIST(Dataset):
+    def __init__(self, config):
+        super(FashionMNIST, self).__init__(config)
+
+        self.mean = np.array([0.2860])
+        self.std = np.array([0.3530])
+
+        self.train_transform = self._get_train_transform()
+        self.test_transform = self._get_default_transform()
+
+    def _get_random_erasing_train_transform(self):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(28, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            transforms.normalize(self.mean, self.std),
+            transforms.random_erasing(
+                self.config['random_erasing_prob'],
+                self.config['random_erasing_area_ratio_range'],
+                self.config['random_erasing_min_aspect_ratio'],
+                self.config['random_erasing_max_attempt']),
+            transforms.to_tensor(),
+        ])
+        return transform
+
+    def _get_cutout_train_transform(self):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(28, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            transforms.normalize(self.mean, self.std),
+            transforms.cutout(self.config['cutout_size'],
+                              self.config['cutout_prob'],
+                              self.config['cutout_inside']),
+            transforms.to_tensor(),
+        ])
+        return transform
+
+    def _get_default_transform(self):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(32, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(self.mean, self.std),
+        ])
+        return transform
+
+    def _get_default_train_transform(self):
+        return self._get_default_transform()
+
+    def _get_default_test_transform(self):
+        return self._get_default_transform()
+
+
 def get_loader(config):
     batch_size = config['batch_size']
     num_workers = config['num_workers']
@@ -167,8 +215,10 @@ def get_loader(config):
 
     if dataset_name in ['CIFAR10', 'CIFAR100']:
         dataset = CIFAR(config)
-    elif dataset_name in ['MNIST', 'FashionMNIST']:
+    elif dataset_name == 'MNIST':
         dataset = MNIST(config)
+    elif dataset_name == 'FashionMNIST':
+        dataset = FashionMNIST(config)
 
     train_dataset, test_dataset = dataset.get_datasets()
 
