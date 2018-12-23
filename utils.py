ab750912250248b1513e03f3e17eaf24c9ca365d
hysts <hysts@users.noreply.github.com>
2018-12-23 11:09:54 +0900

    Clean up

--- utils.py
+++ utils.py
@@ -109,43 +109,6 @@ def create_optimizer(model_parameters, optim_config):
     return optimizer, scheduler
 
 
-def onehot(label, n_classes):
-    return torch.zeros(label.size(0), n_classes).scatter_(
-        1, label.view(-1, 1), 1)
-
-
-def mixup(data, targets, alpha, n_classes):
-    indices = torch.randperm(data.size(0))
-    data2 = data[indices]
-    targets2 = targets[indices]
-
-    targets = onehot(targets, n_classes)
-    targets2 = onehot(targets2, n_classes)
-
-    lam = torch.FloatTensor([np.random.beta(alpha, alpha)])
-    data = data * lam + data2 * (1 - lam)
-    targets = targets * lam + targets2 * (1 - lam)
-
-    return data, targets
-
-
-def cross_entropy_loss(input, target, size_average=True):
-    input = F.log_softmax(input, dim=1)
-    loss = -torch.sum(input * target)
-    if size_average:
-        return loss / input.size(0)
-    else:
-        return loss
-
-
-class CrossEntropyLoss:
-    def __init__(self, size_average=True):
-        self.size_average = size_average
-
-    def __call__(self, input, target):
-        return cross_entropy_loss(input, target, self.size_average)
-
-
 def accuracy(output, target, topk=(1, )):
     with torch.no_grad():
         maxk = max(topk)
