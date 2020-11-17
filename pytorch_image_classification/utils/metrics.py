3e24dce5c5d2e6455656de3b3214a053ea3e90bf
hysts <hysts@users.noreply.github.com>
2020-11-18 07:41:49 +0900

    torch 1.7.0

--- pytorch_image_classification/utils/metrics.py
+++ pytorch_image_classification/utils/metrics.py
@@ -45,6 +45,6 @@ def accuracy(outputs, targets, topk=(1, )):
 
         res = []
         for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(1 / batch_size))
     return res
